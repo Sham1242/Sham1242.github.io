49ebacd129aca41125c11bb68ab8daed43002d4b
Sham1242 <96394328+Sham1242@users.noreply.github.com>
2023-06-14 15:00:57 -0400

    Updates

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const textElement = document.getElementById("cursive-text");
-  const text = "Hello, World!"; // Customize this with the desired text
-
-  textElement.textContent = text;
-});
